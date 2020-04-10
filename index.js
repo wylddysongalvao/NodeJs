2a014c80f4fb9d99d459645cb8920ac9b7115441
Wylddyson Galvão <wylddyson.contato@gmail.com>
2020-04-10 15:42:08 -0300

    Atualização ExpressJs Projeto

--- index.js
+++ index.js
@@ -4,21 +4,62 @@ server = express();
 
 server.use(express.json());
 
-const projetos = {id:"1", Projeto:"Novo Projeto", Task: []};
+// Variável do projeto 
 
+const projetos = {id:"1", Projeto:"Novo Projeto", Tasks: []};
+
+// Criando a Router principal e enviando a menssagem para o navgador.
 server.get('/', (req, res) =>{
   res.send("O Servidor tá de pé galera!");
 })
 
+// Retorna todos os projetos exitentes
 server.get('/projetos', (req, res) => {
   return res.json(projetos);
 })
 
-server.get('/projetos/:index', (req, res) => {
+// Midleware para checar se existe o projeto
+
+function checkProjetosExistentes(req, res, next) {
+  const { id } = req.params;
+  const projeto = projetos.find(p => p.id == id);
+// Condição: Se dentro da rota não encontrar o id ele retorna projeto não encontrado.
+  if (!projeto) {
+    return res.status(400).json({error: 'Projecto Não encontrado'})
+  }
+
+  return next();
+}
+
+// Criando Projeto
+server.post('/projetos', (req, res) => {
+  const { id, title } = req.body;
+
+  const projeto = {
+    id,
+    title,
+    tasks: []
+  };
+  projetos.push(projeto);
+
+  return res.json(projeto);
+});
+
+function logRequests(req, res, next) {
+
+  console.count("Número de requisições");
+
+  return next();
+}
+
+
+server.get('/projetos/:id', (req, res) => {
   const { index } = req.params;
 
-  return res.json(projetos[index]);
+  return res.json(projetos[id]);
 });
+
+server.get('/projetos/')
 
 const porta = 3000
 
